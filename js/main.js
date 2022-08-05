15803124e4045c1f55a8cd8db51edaf480a8f1de
Debayan <pradhandebayan@gmail.com>
2022-08-05 10:20:03 +0530

    updated with new changes

--- js/main.js
+++ js/main.js
@@ -60,7 +60,7 @@ function toggleon(){
 
 document.addEventListener('click', (e)=>{
     if(e.target.classList.contains('link-item') && e.target.hash !== ""){
-        document,querySelector('.overlay').classList.add("active");
+        document.querySelector('.overlay').classList.add("active");
         if(e.target.classList.contains('nav-item')){
             toggleon();
         }
@@ -70,8 +70,8 @@ document.addEventListener('click', (e)=>{
         setTimeout(() =>{
             document.querySelector('section.active').classList.remove("active","fade-out")
             document.querySelector(e.target.hash).classList.add("active");
-            document,querySelector('.overlay').classList.add("active");
             window.scrollTo(0,0);
+            document.querySelector('.overlay').classList.remove("active");
         },500);
     }
 })
